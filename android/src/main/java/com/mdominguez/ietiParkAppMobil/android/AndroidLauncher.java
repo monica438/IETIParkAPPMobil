6c1acd25ae0add7ddef45136813500137fa8a1b9
monica438 <monica14dominguez@gmail.com>
2026-04-23 23:48:07 +0200

    Android Launcher y android gradle arreglados

--- android/src/main/java/com/mdominguez/ietiParkAppMobil/android/AndroidLauncher.java
+++ android/src/main/java/com/mdominguez/ietiParkAppMobil/android/AndroidLauncher.java
@@ -1,15 +1,22 @@
 package com.mdominguez.ietiParkAppMobil.android;
 
+import com.github.czyzby.websocket.CommonWebSockets;
+
 import android.os.Bundle;
+
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 import com.mdominguez.ietiParkAppMobil.GameApp;
 
+
+/** Launches the Android application. */
 public class AndroidLauncher extends AndroidApplication {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        CommonWebSockets.initiate();
         initialize(new GameApp(), configuration);
     }
 }
